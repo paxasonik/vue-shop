5acb14e5e29a7047e00bc10aa322609a769fbae1
paxasonik <paxasonk@mail.ru>
2023-05-16 16:19:47 +0300

    added app layout

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header class="header">
+    Шапка
+  </header>
+
+  <RouterView/>
+
+  <footer class="footer">
+    Подвал
+  </footer>
+</template>
