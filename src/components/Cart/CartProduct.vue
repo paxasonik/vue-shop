afee222f5fb637f5a6b4a5565349a086b07fc4d0
paxasonik <paxasonk@mail.ru>
2023-05-17 14:02:38 +0300

    added cart components

---
+++ src/components/Cart/CartProduct.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <li class="cart__item product">
+    <div class="product__pic">
+      <img src="@/assets/images/catalog/pic-square-3.jpg" width="120" height="120" srcset="@/assets/images/catalog/pic-square-3@2x.jpg 2x" alt="Название товара">
+    </div>
+    <h3 class="product__title">
+      Электрический дрифт-карт Razor Lil’ Crazy
+    </h3>
+    <p class="product__info product__info--color">
+      Цвет:
+      <span>
+        <i style="background-color: #FF6B00"></i>
+        Оранжевый
+      </span>
+    </p>
+    <span class="product__code">Артикул: 1501230</span>
+
+    <div class="product__counter form__counter">
+      <button type="button">
+        <img src="@/assets/images/svg/cart/icon-minus.svg" alt="Убрать один товар" width="10" height="10">
+      </button>
+
+      <input type="text" value="1" name="count">
+
+      <button type="button">
+        <img src="@/assets/images/svg/cart/icon-plus.svg" alt="Добавить один товар" width="10" height="10">
+      </button>
+    </div>
+
+    <b class="product__price">
+      8 990 ₽
+    </b>
+
+    <button class="product__del button-del" type="button">
+      <img src="@/assets/images/svg/cart/icon-close.svg" alt="Удалить товар из корзины" width="20" height="20">
+    </button>
+  </li>
+</template>
--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,9 +1,52 @@
 <script setup lang="ts">
-
+import CartProduct from "@/components/Cart/CartProduct.vue";
 </script>
 
 <template>
+  <main class="content container">
+    <div class="content__top">
+      <ul class="breadcrumbs">
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link" href="index.html">
+            Каталог
+          </a>
+        </li>
+        <li class="breadcrumbs__item">
+          <a class="breadcrumbs__link">
+            Корзина
+          </a>
+        </li>
+      </ul>
 
+      <h1 class="content__title">
+        Корзина
+      </h1>
+      <span class="content__info">3 товара</span>
+    </div>
+
+    <section class="cart">
+      <form class="cart__form form" action="#" method="POST">
+        <div class="cart__field">
+          <ul class="cart__list">
+            <cart-product/>
+          </ul>
+        </div>
+
+        <div class="cart__block">
+          <p class="cart__desc">
+            Мы&nbsp;посчитаем стоимость доставки на&nbsp;следующем этапе
+          </p>
+          <p class="cart__price">
+            Итого: <span>32 970 ₽</span>
+          </p>
+
+          <router-link :to="{ name: 'order' }" class="cart__button button button--primery">
+            Оформить заказ
+          </router-link>
+        </div>
+      </form>
+    </section>
+  </main>
 </template>
 
 <style scoped>
